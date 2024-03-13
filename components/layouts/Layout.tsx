72901f1c042ac19cedd2679772a6486f876ab594
LDS196 <lihachevd87@gmail.com>
2024-03-13 12:37:37 +0400

    feat: SizeTable.tsx

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -5,15 +5,21 @@ import MobileNavbar from '@/components/modules/MobileNavbar/MobileNavbar'
 import { AnimatePresence, motion } from 'framer-motion'
 import SearchModal from '@/components/modules/Header/SearchModal'
 import { useUnit } from 'effector-react'
-import { $searchModal, $showQuickViewModal } from '@/context/modals'
+import {
+  $searchModal,
+  $showQuickViewModal,
+  $showSizeTable,
+} from '@/context/modals'
 import { handleCloseSearchModal } from '@/lib/utils/common'
 import Footer from '@/components/modules/Footer/Footer'
 import QuickViewModal from '@/components/modules/QuickViewModal/QuickViewModal'
+import SizeTable from '@/components/modules/SizeTable/SizeTable'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const isMedia800 = useMediaQuery(800)
   const searchModal = useUnit($searchModal)
   const showQuickViewModal = useUnit($showQuickViewModal)
+  const showSizeTable = useUnit($showSizeTable)
 
   return (
     <>
@@ -28,6 +34,15 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             exit={{ opacity: 0 }}
           >
             <SearchModal />
+          </motion.div>
+        )}
+        {showSizeTable && (
+          <motion.div
+            initial={{ opacity: 0, zIndex: 102 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <SizeTable />
           </motion.div>
         )}
       </AnimatePresence>
